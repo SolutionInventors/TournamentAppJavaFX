c4c79be4f5d7e0059fab94a8eacfab40b5161a2b
ChineduKnight <eduboy93@yahoo.com>
2017-08-02 21:38:38 +0100

    first change to the Java FX

---
+++ TournamentAppJavaFX/src/com/solutioninventors/tournament/Tournament.java
@@ -0,0 +1,17 @@
+/**
+ * Jul 27, 2017
+ * @author Chidiebere
+ * Tournament.java
+ * 8:07:23 AM
+ */
+
+package com.solutioninventors.tournament;
+
+public class Tournament
+{
+	
+	public static void main( String[] args )
+	{
+		System.out.println( "Hello World" );
+	}
+}
