894028d7aa5a55ef66982ec22e5fa5817fd71e37
Chinedu <eduboy@yahoo.com>
2017-08-10 12:35:58 +0100

    alignment

--- TournamentAppJavaFX/src/com/solutioninventors/tournament/test/Test.java
+++ TournamentAppJavaFX/src/com/solutioninventors/tournament/test/Test.java
@@ -11,63 +11,55 @@ import javax.swing.JOptionPane;
 import com.solutioninventors.tournament.utils.Fixture;
 import com.solutioninventors.tournament.utils.Round;
 
-public class Test
-{
-	public static void displayStandingTable(String[][] stringTable)
-	{
-		StringBuilder builder = new StringBuilder( 1000 );
-		
-		if ( stringTable[ 0 ].length == 8 )
-			builder.append(  "Name    W D L GF GA GD Pts\n" );
+public class Test {
+	public static void displayStandingTable(String[][] stringTable) {
+		StringBuilder builder = new StringBuilder(1000);
+
+		if (stringTable[0].length == 8)
+			builder.append("Name    W D L GF GA GD Pts\n");
 		else
-			builder.append( "Name     W D L Pts\n" );
-		
-		for( int row = 0 ; row < stringTable.length ; row++ )
-		{
-			for( int col = 0 ; col < stringTable[row].length ; col ++)
-				builder.append( String.format("%s ", stringTable[row][ col ] ));
-			
-			builder.append( "\n");
-				
+			builder.append("Name     W D L Pts\n");
+
+		for (int row = 0; row < stringTable.length; row++) {
+			for (int col = 0; col < stringTable[row].length; col++)
+				builder.append(String.format("%s ", stringTable[row][col]));
+
+			builder.append("\n");
+
 		}
-		displayMessage("The table is \n" +  builder.toString() );;
-			
+		displayMessage("The table is \n" + builder.toString());
+		;
+
 	}
 
-	public static void displayFixtures(Fixture[] currentFixtures)
-	{
-		StringBuilder builder = new StringBuilder(500 );
-		
-		for ( int i = 0 ; i < currentFixtures.length ; i++ )
-			builder.append(String.format( "%s VS %s\n" ,
-					currentFixtures[ i ].getCompetitorOne() , currentFixtures[ i ].getCompetitorTwo() ));
-		
-		displayMessage( builder.toString() );
-		
-		
+	public static void displayFixtures(Fixture[] currentFixtures) {
+		StringBuilder builder = new StringBuilder(500);
+
+		for (int i = 0; i < currentFixtures.length; i++)
+			builder.append(String.format("%s VS %s\n", currentFixtures[i].getCompetitorOne(),
+					currentFixtures[i].getCompetitorTwo()));
+
+		displayMessage(builder.toString());
+
 	}
 
-	public static void displayMessage(String message) 
-	{
-		System.out.println( message );
-		JOptionPane.showMessageDialog(null ,message );
+	public static void displayMessage(String message) {
+		System.out.println(message);
+		JOptionPane.showMessageDialog(null, message);
 	}
 
-	public static void displayRoundResults(Round currentRound)
-	{
+	public static void displayRoundResults(Round currentRound) {
 		Fixture[] fixtures = currentRound.getFixtures();
-		
-		StringBuilder builder = new StringBuilder(500 );
-		builder.append( "Round Results: " );
-		for (Fixture fixture : fixtures)
-		{
-			builder.append(String.format( "%s %.1f VS %.1f %s\n" ,
-					fixture.getCompetitorOne() , fixture.getCompetitorOneScore(),
-					fixture.getCompetitorTwoScore() , fixture.getCompetitorTwo() ));
+
+		StringBuilder builder = new StringBuilder(500);
+		builder.append("Round Results: ");
+		for (Fixture fixture : fixtures) {
+			builder.append(String.format("%s %.1f VS %.1f %s\n", fixture.getCompetitorOne(),
+					fixture.getCompetitorOneScore(), fixture.getCompetitorTwoScore(), fixture.getCompetitorTwo()));
 		}
-			
-		displayMessage( builder.toString() );
-		
+
+		displayMessage(builder.toString());
+
 	}
 
 }
--- TournamentAppJavaFX/src/com/solutioninventors/tournament/utils/Fixture.java
+++ TournamentAppJavaFX/src/com/solutioninventors/tournament/utils/Fixture.java
@@ -2,156 +2,122 @@ package com.solutioninventors.tournament.utils;
 
 import javax.swing.JOptionPane;
 
-public class Fixture
-{
-	
+public class Fixture {
+
 	private final Competitor COMPETITOR_ONE;
 	private final Competitor COMPETITOR_TWO;
-	private  double competitorOneScore;
-	private  double competitorTwoScore ;
-	private boolean complete ;
-	
-	public Fixture(Competitor competitorOne, Competitor competitorTwo)
-	{
-		COMPETITOR_ONE = competitorOne ;
-		COMPETITOR_TWO = competitorTwo ;
-		
+	private double competitorOneScore;
+	private double competitorTwoScore;
+	private boolean complete;
+
+	public Fixture(Competitor competitorOne, Competitor competitorTwo) {
+		COMPETITOR_ONE = competitorOne;
+		COMPETITOR_TWO = competitorTwo;
+
 	}
 
-	
-	public void setResult( final double score1 , final double score2 ) 
-	{
-		if ( !isComplete() )
-		{
-			if ( score1 > score2 )
-			{
+	public void setResult(final double score1, final double score2) {
+		if (!isComplete()) {
+			if (score1 > score2) {
 				getCompetitorOne().incrementWins();
 				getCompetitorTwo().incrementLoss();
-			}
-			else if ( score1 == score2  ) //match is a draw
+			} else if (score1 == score2) // match is a draw
 			{
 				getCompetitorOne().incrementDraw();
 				getCompetitorTwo().incrementDraw();
-			}
-			else
-			{
+			} else {
 				getCompetitorOne().incrementLoss();
-				getCompetitorTwo(). incrementWins();
+				getCompetitorTwo().incrementWins();
 			}
-			
-			COMPETITOR_ONE.incrementGoalsScoredBy( score1 );
-			COMPETITOR_ONE.incrementGoalsConcededBy( score2 );
-			COMPETITOR_TWO.incrementGoalsScoredBy( score2 );
-			COMPETITOR_TWO.incrementGoalsConcededBy( score1 );
-			
-			competitorOneScore = score1 ;
-			competitorTwoScore = score2 ;
-			
-			complete = true ;
-		}
-		else
-		{
-			String message = String.format( "%s\n%s %.1f VS %.1f %s", "This fixture has a result" ,
-										getCompetitorOne().getName() , getCompetitorOneScore() , 
-										getCompetitorTwoScore() , getCompetitorTwo().getName() );
-			JOptionPane.showMessageDialog( null , message );
-			
+
+			COMPETITOR_ONE.incrementGoalsScoredBy(score1);
+			COMPETITOR_ONE.incrementGoalsConcededBy(score2);
+			COMPETITOR_TWO.incrementGoalsScoredBy(score2);
+			COMPETITOR_TWO.incrementGoalsConcededBy(score1);
+
+			competitorOneScore = score1;
+			competitorTwoScore = score2;
+
+			complete = true;
+		} else {
+			String message = String.format("%s\n%s %.1f VS %.1f %s", "This fixture has a result",
+					getCompetitorOne().getName(), getCompetitorOneScore(), getCompetitorTwoScore(),
+					getCompetitorTwo().getName());
+			JOptionPane.showMessageDialog(null, message);
+
 		}
 	}
 
-	public boolean isComplete()
-	{
+	public boolean isComplete() {
 		return complete;
 	}
 
-
-	public boolean isDraw()
-	{
-		if ( isComplete() && ( getCompetitorOneScore() == getCompetitorTwoScore() ) )
-			return true ;
-		else
-			return false ;
-	}
-	public boolean hasWinner()
-	{
-		if ( isComplete() && ( getCompetitorOneScore() > getCompetitorTwoScore() && isDraw() )  )
-			return true ;
-		else
-			return false ;
-	}
-	
-	public boolean hasLoser()
-	{
-		return hasWinner() ;
-	}
-	
-	public Competitor getWinner()
-	{
-		if ( hasWinner() )
-			return getCompetitorOneScore() > getCompetitorTwoScore() ? 
-					getCompetitorOne() : getCompetitorTwo() ;
-					
-		return null ;
-		
-	}
-	
-	public Competitor getLoser()
-	{
-		if ( hasLoser() )
-			return getCompetitorOneScore() < getCompetitorTwoScore() ? 
-					getCompetitorOne() : getCompetitorTwo() ;
-					
-		return null ;
-		
-	}
-	
-	public boolean hasFixture( Competitor com1  , Competitor com2 )
-	{
-//		returns true if com1 equals  COMPETITOR_ONE AND con2 = COMPETITOR_TWO
-		if ( getCompetitorOne().getName().equals( com1.getName()  ) && 
-			 getCompetitorTwo().getName().equals( com2.getName() ))
+	public boolean isDraw() {
+		if (isComplete() && (getCompetitorOneScore() == getCompetitorTwoScore()))
 			return true;
 		else
 			return false;
 	}
-	
-	
-	public Competitor getCompetitorOne()
-	{
-		return COMPETITOR_ONE ;
-	}
-	
-	public Competitor getCompetitorTwo()
-	{
-		return COMPETITOR_TWO ;
+
+	public boolean hasWinner() {
+		if (isComplete() && (getCompetitorOneScore() > getCompetitorTwoScore() && isDraw()))
+			return true;
+		else
+			return false;
 	}
 
+	public boolean hasLoser() {
+		return hasWinner();
+	}
 
-	public double getCompetitorOneScore()
-	{
+	public Competitor getWinner() {
+		if (hasWinner())
+			return getCompetitorOneScore() > getCompetitorTwoScore() ? getCompetitorOne() : getCompetitorTwo();
+
+		return null;
+
+	}
+
+	public Competitor getLoser() {
+		if (hasLoser())
+			return getCompetitorOneScore() < getCompetitorTwoScore() ? getCompetitorOne() : getCompetitorTwo();
+
+		return null;
+
+	}
+
+	public boolean hasFixture(Competitor com1, Competitor com2) {
+		// returns true if com1 equals COMPETITOR_ONE AND con2 = COMPETITOR_TWO
+		if (getCompetitorOne().getName().equals(com1.getName()) && getCompetitorTwo().getName().equals(com2.getName()))
+			return true;
+		else
+			return false;
+	}
+
+	public Competitor getCompetitorOne() {
+		return COMPETITOR_ONE;
+	}
+
+	public Competitor getCompetitorTwo() {
+		return COMPETITOR_TWO;
+	}
+
+	public double getCompetitorOneScore() {
 		return competitorOneScore;
 	}
 
-
-	public double getCompetitorTwoScore()
-	{
+	public double getCompetitorTwoScore() {
 		return competitorTwoScore;
 	}
 
-
-	public void setResult(double score1, double score2, boolean b)
-	{
-		if ( b )
+	public void setResult(double score1, double score2, boolean b) {
+		if (b)
 			setResult(score1, score2);
-		else
-		{
+		else {
 			competitorOneScore = score1;
 			competitorTwoScore = score2;
 		}
-		
+
 	}
 
-
-	
-	
 }
