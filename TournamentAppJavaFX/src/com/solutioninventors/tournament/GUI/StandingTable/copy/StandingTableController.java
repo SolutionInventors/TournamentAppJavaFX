6b4ef94e5cc32989231dbf1f3fda2f2dfca87e8b
Chinedu <eduboy@yahoo.com>
2017-08-15 14:40:00 +0100

    still battling with the list of list

--- TournamentAppJavaFX/src/com/solutioninventors/tournament/GUI/StandingTable/copy/StandingTableController.java
+++ TournamentAppJavaFX/src/com/solutioninventors/tournament/GUI/StandingTable/copy/StandingTableController.java
@@ -4,6 +4,7 @@
 package com.solutioninventors.tournament.GUI.StandingTable.copy;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javax.swing.text.StyledEditorKit.ForegroundAction;
@@ -13,7 +14,6 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TableColumn;
@@ -33,7 +33,7 @@ public class StandingTableController implements Initializable {
 	TableColumn<StandingTable, Double> WDLFADP[][];
 	String tableLabel[] = {"P","W","D","L","GF","GA","GD","PTS"};
 	String standingTableValue[] = {"gamesPlayed","wins","draw","loss","goalsFor","goalsAgainst","goalsDiff","points"};
-	int noOfRounds = 3;
+	int noOfRounds = 2;
 	private VBox vBox;
 	
 	@SuppressWarnings("unchecked")
@@ -55,26 +55,31 @@ public class StandingTableController implements Initializable {
 				}
 			}
 
-		ObservableList<StandingTable> list = FXCollections.observableArrayList(
+	/*	ObservableList<StandingTable> list = FXCollections.observableArrayList(
 				
 				new StandingTable("Manu", 5, 2, 4, 3, 4, 2, 3, 4), 
 				new StandingTable("Arsenal", 5, 2, 4, 3, 4, 2, 3, 4),
 				new StandingTable("Chelsea", 5, 2, 4, 3, 4, 2, 3, 4)
 
 		);
-		ObservableList<StandingTable> list2 = FXCollections.observableArrayList(
+			ObservableList<StandingTable> list2 = FXCollections.observableArrayList(
 
 				new StandingTable("Everton", 5, 2, 4, 3, 4, 2, 3, 4),
 				new StandingTable("Liverpool", 5, 2, 4, 3, 4, 2, 3, 4), 
 				new StandingTable("ManCity", 5, 2, 4, 3, 4, 2, 3, 4)
 
 		);
+		*/
+		List<ObservableList<StandingTable>> abc;
+		abc = setuptablevariable();
+	
+	
 		table = new TableView[noOfRounds];
 		
 		//how to pass in multiple list
 		for (int i = 0; i < table.length; i++) {
 			table[i] = new TableView<>();
-			table[i].setItems(list);
+			table[i].setItems(abc.get(i));
 			table[i].getColumns().add(name[i]);
 			for (int col = 0; col < WDLFADP[i].length; col++) {
 				table[i].getColumns().add(WDLFADP[i][col]);
@@ -82,6 +87,34 @@ public class StandingTableController implements Initializable {
 		}
 	}// end table setup
 
+	public List<ObservableList<StandingTable>> setuptablevariable() {
+		List<ObservableList<StandingTable>> aaa =FXCollections.observableArrayList();
+		List<ObservableList<StandingTable>> abc =FXCollections.observableArrayList();
+		
+		ObservableList<StandingTable> items = FXCollections.observableArrayList();
+		int noOfComp = 2;
+		String nameofcomp[] = {"Everton","Liverpool","Arsenal","Chelsea"};
+		String nameofcomp2[] = {"Arsenal","Chelsea"};
+		StandingTable a;
+		a=new StandingTable(nameofcomp2[1], 5, 2, 4, 3, 4, 2, 3, 4);
+		
+		for (int i = 0; i < noOfRounds; i++) {
+			for (int j = 0; j < noOfComp; j++) {
+				if (i>0) {
+					int b = j+2;
+					//abc.add(new StandingTable(nameofcomp2[j], 5, 2, 4, 3, 4, 2, 3, 4));
+				}else
+					items.add(new StandingTable(nameofcomp[j], 5, 2, 4, 3, 4, 2, 3, 4));
+				
+			}
+			
+			aaa.add(i, abc.get(i));
+			items.clear();
+		}//end for loop
+		
+		return aaa;
+		
+	}
 
 
 	@Override
