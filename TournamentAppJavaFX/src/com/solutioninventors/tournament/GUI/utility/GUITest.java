f311bbd686bb4cd0eefae8cd0ed11a4320a99b6b
ChineduKnight <eduboy93@yahoo.com>
2017-08-26 20:22:17 +0100

    Welcome Screen
    Welcome Screen has been designed and implemented. Work is about to start
    on Tournament selection Screen

--- TournamentAppJavaFX/src/com/solutioninventors/tournament/GUI/utility/GUITest.java
+++ TournamentAppJavaFX/src/com/solutioninventors/tournament/GUI/utility/GUITest.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * @author ChineduKnight
@@ -21,8 +22,10 @@ public class GUITest  extends Application{
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource(Paths.viewpath+"WelcomeScreen.fxml"));
 			Scene scene = new Scene(root);
+			primaryStage.initStyle(StageStyle.TRANSPARENT);
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 			scene.getStylesheets().add(getClass().getResource(Paths.viewpath+"lookfeel.css").toExternalForm());
 			primaryStage.setTitle("Tournament APP");
 		} catch(Exception e) {
