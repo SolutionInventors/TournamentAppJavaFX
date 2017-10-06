517c435baefa2a8c6e6a2003c22aa6e66fcd909e
Chidiebere <Chidiebere@Chidi>
2017-10-06 12:37:35 +0100

    Adding to index

---
+++ TournamentAppJavaFX/src/com/solutioninventors/tournament/types/package-info.java
@@ -0,0 +1,9 @@
+/**
+ *@author Oguejiofor Chidiebere
+ *package-info.java
+ *Aug 5, 2017
+ *8:06:32 AM
+ *
+ *This package declares classes that ontain the several types of {@link Tournament}s
+ */
+package com.solutioninventors.tournament.types;
